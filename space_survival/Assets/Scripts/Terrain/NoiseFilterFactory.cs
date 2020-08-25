20088f9d8d3b1b93b23c76420e4916cace9651a7
Brad Gibb <gorathon@gmail.com>
2020-08-25 09:39:53 +0200

    Origianl Commit

---
+++ space_survival/Assets/Scripts/Terrain/NoiseFilterFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NoiseFilterFactory
+{
+    public static INoiseFilter CreateNoiseFilter(NoiseSettings noiseSettings)
+    {
+        switch (noiseSettings.filterType)
+        {
+            case NoiseSettings.FilterType.Simple:
+                return new SimpleNoiseFilter(noiseSettings.simpleNoiseSettings);
+            case NoiseSettings.FilterType.Ridgid:
+                return new RidgidNoiseFilter(noiseSettings.ridgidNoiseSettings);
+        }
+        return null;
+    }
+}
---
+++ space_survival/Assets/Scripts/Terrain/NoiseSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings
+{
+    public enum FilterType { Simple, Ridgid };
+    public FilterType filterType;
+
+    [ConditionalHide("filterType", 0)]
+    public SimpleNoiseSettings simpleNoiseSettings;
+    [ConditionalHide("filterType", 1)]
+    public RidgigNoiseSettings ridgidNoiseSettings;
+
+    [System.Serializable]
+    public class SimpleNoiseSettings
+    {
+        public float strength = 1;
+        [Range(1,8)]
+        public int numberOfLayers = 1;
+        public float baseRoughness = 1;
+        public float roughness = 2;
+        public float persistance = 0.5f;
+        public Vector3 centre;
+        public float minValue;
+    }
+
+    [System.Serializable]
+    public class RidgigNoiseSettings : SimpleNoiseSettings
+    {
+        public float weightMultiplier = 0.8f;
+    }
+}
